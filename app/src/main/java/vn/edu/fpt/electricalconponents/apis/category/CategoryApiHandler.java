b9b11785f2f90e9a3192627bf2a5f2a7264bfb4a
neru <ngocngocthuc@gmail.com>
2025-03-23 23:13:43 +0700

    implement category api handler

--- app/src/main/java/vn/edu/fpt/electricalconponents/apis/category/CategoryApiHandler.java
+++ app/src/main/java/vn/edu/fpt/electricalconponents/apis/category/CategoryApiHandler.java
@@ -1,24 +1,50 @@
 package vn.edu.fpt.electricalconponents.apis.category;
 
 import android.annotation.SuppressLint;
-import android.content.Context;
 
+import java.util.List;
+import java.util.MissingFormatArgumentException;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 import vn.edu.fpt.electricalconponents.apis.HttpClient;
+import vn.edu.fpt.electricalconponents.models.Category;
 
 public class CategoryApiHandler {
     @SuppressLint("StaticFieldLeak")
     private static CategoryApiHandler instance;
     private final String originalUrl;
-    private final Context context;
     private HttpClient httpClient;
     private CategoryApi categoryApi;
-    private CategoryApiHandler(Context context) {
+
+    private CategoryApiHandler() {
         originalUrl = "api/category/";
-        this.context = context;
     }
-    public static CategoryApiHandler getInstance(Context context) {
+
+    public static CategoryApiHandler getInstance() {
         if (instance == null) {
-            instance = new CategoryApiHandler(context);
+            instance = new CategoryApiHandler();
         }
         return instance;
+    }
+
+    public Observable<List<Category>> getAllCategories() {
+        httpClient.openClient(originalUrl);
+        categoryApi = httpClient.getClient().create(CategoryApi.class);
+        Observable<List<Category>> observable
+                = categoryApi.getAllCategories()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .map(response -> {
+                    if (response == null || response.getData() == null)
+                        throw new MissingFormatArgumentException("Get all categories failed: Response body is empty");
+                    if (response.getStatus() != 200)
+                        throw new MissingFormatArgumentException("Get all categories failed: " + response.getMessage());
+                    return response.getData();
+                });
+        categoryApi = null;
+        httpClient.closeClient();
+        return observable;
+    }
 }
